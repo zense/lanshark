4044f8ba0d59813626e25a945de89294e3b983df
Aditya Shridhar Hegde <adishegde@gmail.com>
2018-02-14 02:01:49 +0530

    Start lanshark daemon in main process
    1. Extract daemon and client functions into utils/lansharkDaemon.js and
    utils/lansharkClient.js respectively so that lansharkDaemon can be used in
    main process easily.
    2. Extract runPythonScript from utils/lansharkInterface.js into
    utils/pythonScrape.js
    3. Start daemon in main process
    4. Update the URL loaded into window to handle production and
    development environments.

--- src/main.js
+++ src/main.js
@@ -1,29 +1,40 @@
+import {
+    app, // module to control application life
+    BrowserWindow // module to create native browser windows
+} from 'electron';
+import path from 'path';
+import url from 'url';
+import { startDaemon } from 'utils/lansharkDaemon';
+
 /*
-   This is the entry point for the electron process
+  This is the entry point for the electron main process
 */
 
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+// The URL of HTML file served by webpack-dev-server
+const DEV_URL = 'http://localhost:1234/';
+// URI of HTML file in production
+const PROD_URL = url.format({
+    pathname: path.join(__dirname, '../public/index.html'),
+    protocol: 'file:',
+    slashes: true
+});
+// In development mode use file served by webpack-dev-server
+const LOADURL = (
+    process.env.ELECTRON_ENV == 'development'
+) ? DEV_URL : PROD_URL;
 
-const path = require('path');
-const url = require('url');
+global.pathToSrc = __dirname;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+var mainWindow;
 
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:1234');
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    mainWindow.loadURL(LOADURL);
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
@@ -34,10 +45,17 @@ function createWindow() {
     });
 }
 
+function startApp() {
+    // Start lanshark daemon
+    startDaemon();
+    // Create new window
+    createWindow();
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', startApp);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -55,6 +73,3 @@ app.on('activate', function () {
         createWindow();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
---
+++ src/utils/pythonScript.js
@@ -0,0 +1,44 @@
+import { spawn } from 'child_process';
+import * as log from 'loglevel';
+
+/*
+  Wrapper function to run python scripts.
+
+  Arguments:
+      Prameters to child_process.spawn
+
+  Return Value:
+      A promise that is resolved when an error occurs or when the script is
+      completely run. On success promise resolves to output else promise is
+      resolved to error.
+*/
+export default function runPythonScript(params) {
+    return new Promise(function(resolve, reject) {
+        var shell = false;
+        // Creating a shell is required in Windows. In UNIX systems
+        // its faster if 'shell' is set to 'false'.
+        if (process.platform === "win32") shell = true;
+
+        log.debug(
+            "Command executed in runPythonScript: ",
+            `python ${params}`
+        );
+
+        spawn("python", params, {
+            shell: shell
+        }).on("exit", function() {
+            // On exit event extract output
+            var output = this.stdout.read();
+
+            // If output exists then convert it to string
+            // and resolve Promise to output
+            if (output)
+                resolve(output.toString());
+            else resolve("");
+        }).on("error", function(err) {
+            // An 'exit' event might not be emitted for all errors
+            // In case of error reject Promise
+            reject(err);
+        });
+    });
+}
