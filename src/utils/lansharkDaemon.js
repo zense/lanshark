2e09ecf22c2b718310b287d358021e43063073da
Aditya Shridhar Hegde <adishegde@gmail.com>
2018-02-14 02:01:49 +0530

    Start lanshark daemon in main process
    1. Extract daemon and client functions into utils/lansharkDaemon.js and
    utils/lansharkClient.js respectively so that lansharkDaemon can be used in
    main process easily.
    2. Extract runPythonScript from utils/lansharkInterface.js into
    utils/pythonScrape.js
    3. Start daemon in main process
    4. Update the URL loaded into window to handle production and
    development environments.

---
+++ src/utils/lansharkDaemon.js
@@ -0,0 +1,32 @@
+import runPythonScript from 'utils/pythonScript';
+import path from 'path';
+import * as log from 'loglevel';
+
+/*
+  This script is used to manage the lanshark daemon. This script is usually made
+  use of in the main process.
+*/
+
+// resources uses remote to correctly set path for render process thus it won't
+// work in main process. Since this script is usually called within the main
+// process we can use __dirname
+const PATH_DAEMON = path.join(__dirname, 'lanshark', 'daemon.py');
+
+/*
+  Starts the daemon in the background.
+
+  Return Value:
+  It returns a Promise that is resolved when the program finishes execution.
+  Since the daemon program (python script) redirects I/O automatically the
+  Promise is not resovled to any value nor is it rejected on error.
+
+  TODO: Fix error handling and I/O
+*/
+export function startDaemon() {
+    return runPythonScript([PATH_DAEMON]).then(function() {
+        log.info(
+            "lansharkInterface.js/startDaemon: Start daemon script executed."
+        );
+    });
+}
+
--- src/utils/resources.js
+++ src/utils/resources.js
@@ -1,15 +1,23 @@
-import path from "path";
+import { remote } from 'electron';
+import path from 'path';
+import log from 'loglevel';
 
 /*
   This file contains values of some resources to be used everywhere else.
 
   The following named exports are made:
-    - lansharkd: Path to utils/lanshark/client.py program.
-    - lansharkc: Path to utils/lanshark/daemon.py program.
+  - lansharkd: Path to utils/lanshark/client.py program.
+  - lansharkc: Path to utils/lanshark/daemon.py program.
 */
 
-// Path to folder where lanshard and lansharc programs are present
-var pathLansharkBinary = path.join(__dirname, 'lanshark');
+// Get path to src from main.js so that resource paths can be constructed
+const pathToSrc = remote.getGlobal('pathToSrc');
 
-export var lansharkd = path.join(pathLansharkBinary, 'daemon.py');
-export var lansharkc = path.join(pathLansharkBinary, 'client.py');
+// Path to folder where lanshard and lansharc programs are present
+const pathLansharkBinary = path.join(pathToSrc, 'lanshark');
+
+export const lansharkd = path.join(pathLansharkBinary, 'daemon.py');
+log.debug("util/resources.js lansharkd: ", lansharkd);
+
+export const lansharkc = path.join(pathLansharkBinary, 'client.py');
+log.debug("util/resources.js lansharkc: ", lansharkc);
